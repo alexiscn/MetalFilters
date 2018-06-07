da5ce49ee088f98dcd5d4f17ced8ef74de712d40
xu.shuifeng <xushuifeng@xushuifengdeMacBook-Pro.local>
2018-06-07 15:37:54 +0800

    add IFFilter protocol

---
+++ MetalFilters/InstaFilters/IFFilter.swift
@@ -0,0 +1,16 @@
+//
+//  IFFilter.swift
+//  MetalFilters
+//
+//  Created by xu.shuifeng on 2018/6/7.
+//  Copyright © 2018 shuifeng.me. All rights reserved.
+//
+
+import Foundation
+
+protocol IFFilter {
+    
+    var name: String { get }
+    
+    var entrance: String { get }
+}
